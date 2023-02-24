27fce24c4e248aeafa8d534597fe62862f0b01dd
antionette238 <125678622+antionette238@users.noreply.github.com>
2023-02-24 05:26:15 -0800

    Add files via upload

---
+++ Computer Assignment 2.py
@@ -0,0 +1,142 @@
+#Annette Motors
+print("Welcome to Annette's motors")
+name= input("Please what is your name?")
+print()
+print("Hello" + name +",We would display our carlist shortly ")
+carlist=("mercedes benz,honda civic,honda accord,toyota corolla,ford explorer,audi,kia picanto\
+,g-wagon,maseratti,bugatti,jeep,RAM,tesla,toyota camry,porsche,lamborghini,ferrari,\
+rolls royce,lexus,GMC,mitsubishi,suzuki,renault,volkswagen,toyota Rav4,chevrolet,\
+ toyota vitz,hyundai,toyota tundra,katanka")
+print()
+print(name + ",The cars available are:"+ carlist)
+print("Please what car would you like to purchase")
+
+car=input()
+
+if car=="mercedes benz" or car=="Mercedes Benz":
+    print("price=45000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="honda civic" or car=="Honda civic":
+    print("price=35000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="honda accord" or car=="Honda accord":
+    print("price=34500")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="toyota corolla"or car=="Toyota corolla":
+    print("price=25000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="ford explorer"or car=="Ford explorer":
+    print("price=75000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="audi" or car=="Audi":
+    print("price=85000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="kia picanto"or car=="Kia picanto":
+    print("price=15000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="g-wagon" or car=="G-wagon":
+    print("price=46000")   
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="maseratti" or car=="Maseratti":
+    print("price=74000")    
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="bugatti"or car=="Bugatti":
+    print("price=86000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="jeep" or car=="Jeep":
+    print("price=90000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="RAM" or car=="ram":
+    print("price=72000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")    
+elif car=="tesla" or car=="Tesla":
+    print("price=96000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="toyota camry" or car=="Tesla":
+    print("price=37000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="porsche" or car=="Porsche":
+    print("price=80000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="lamborghini" or car=="Lamborghini":
+    print("price=92000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="ferrari" or car=="Ferrari":
+    print("price=91000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="rolls royce" or car=="Rolls royce":
+    print("price=55000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="lexus" or car=="Lexus":
+    print("price=48000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="GMC" or car=="gmc":
+    print("price=75500")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="mitsubishi"or car=="Mitsubishi":
+    print("price=56000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="suzuki" or car=="Suzuki":
+    print("price=35000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="renault" or car=="Renault":
+    print("price=35000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="volkswagen" or car=="Volkswagen":
+    print("price=39500")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="toyota Rav4"or car=="Toyota rav4":
+    print("price=62450")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="chevrolet"or car=="Chevrolet":
+    print("price=76300")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="toyota vitz" or car=="Toyota vitz":
+    print("price=18000")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="hyundai" or car=="Hyundai":
+    print("price=41500")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="toyota tundra" or car=="Toyota tundra":
+    print("price=81250")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+elif car=="katanka"or car=="Katanka":
+    print("price=50500")
+    print("Please your" + car +",will be made available soon,please move into the accountant's office to make payments")
+       
+else:
+    print("Sorry!"+ name + ",we don't have any of those available currently.")
+
+  
+
+    
+    
+
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
